68e91788998e323ebaa53b2e411db95b00756ab1
ACER <ACER@DESKTOP-JDQM4BO>
2024-10-08 00:04:11 +0300

    Add project files.

---
+++ Лаб 4  ! 2 !/.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Лаб_4____2__
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAdd = new System.Windows.Forms.ToolStripButton();
+            this.btnEdit = new System.Windows.Forms.ToolStripButton();
+            this.tsSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnDel = new System.Windows.Forms.ToolStripButton();
+            this.btnClear = new System.Windows.Forms.ToolStripButton();
+            this.tsSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnExit = new System.Windows.Forms.ToolStripButton();
+            this.gvPlanes = new System.Windows.Forms.DataGridView();
+            this.bindSrcPlanes = new System.Windows.Forms.BindingSource(this.components);
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvPlanes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindSrcPlanes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAdd,
+            this.btnEdit,
+            this.tsSeparator1,
+            this.btnDel,
+            this.btnClear,
+            this.tsSeparator2,
+            this.btnExit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(900, 29);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
+            this.btnAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(34, 24);
+            this.btnAdd.Text = "Додати запис про літак ";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnEdit.Image = ((System.Drawing.Image)(resources.GetObject("btnEdit.Image")));
+            this.btnEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(34, 24);
+            this.btnEdit.Text = "Редагувати запис";
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // tsSeparator1
+            // 
+            this.tsSeparator1.Name = "tsSeparator1";
+            this.tsSeparator1.Size = new System.Drawing.Size(6, 29);
+            // 
+            // btnDel
+            // 
+            this.btnDel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnDel.Image = ((System.Drawing.Image)(resources.GetObject("btnDel.Image")));
+            this.btnDel.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(34, 24);
+            this.btnDel.Text = "Видалити запис ";
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
+            this.btnClear.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(34, 24);
+            this.btnClear.Text = "Очистити дані ";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // tsSeparator2
+            // 
+            this.tsSeparator2.Name = "tsSeparator2";
+            this.tsSeparator2.Size = new System.Drawing.Size(6, 29);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(34, 24);
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // gvPlanes
+            // 
+            this.gvPlanes.AllowUserToAddRows = false;
+            this.gvPlanes.AllowUserToDeleteRows = false;
+            this.gvPlanes.AutoGenerateColumns = false;
+            this.gvPlanes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvPlanes.DataSource = this.bindSrcPlanes;
+            this.gvPlanes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gvPlanes.Location = new System.Drawing.Point(0, 29);
+            this.gvPlanes.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gvPlanes.Name = "gvPlanes";
+            this.gvPlanes.ReadOnly = true;
+            this.gvPlanes.RowHeadersWidth = 51;
+            this.gvPlanes.RowTemplate.Height = 24;
+            this.gvPlanes.Size = new System.Drawing.Size(900, 533);
+            this.gvPlanes.TabIndex = 1;
+            this.gvPlanes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gvPlanes_CellContentClick);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.gvPlanes);
+            this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота 4 ";
+            this.Load += new System.EventHandler(this.fMain_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvPlanes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindSrcPlanes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnAdd;
+        private System.Windows.Forms.ToolStripButton btnEdit;
+        private System.Windows.Forms.ToolStripSeparator tsSeparator1;
+        private System.Windows.Forms.ToolStripButton btnDel;
+        private System.Windows.Forms.ToolStripButton btnClear;
+        private System.Windows.Forms.ToolStripSeparator tsSeparator2;
+        private System.Windows.Forms.ToolStripButton btnExit;
+        private System.Windows.Forms.BindingSource bindSrcPlanes;
+        private System.Windows.Forms.DataGridView gvPlanes;
+    }
+}
+
---
+++ Лаб 4  ! 2 !/fMain.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Лаб_3___2__;
+
+namespace Лаб_4____2__
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int buttonsSize = 5 * btnAdd.Width + 2 * tsSeparator1.Width + 30;
+            btnExit.Margin = new Padding(Width - buttonsSize, 0, 0, 0);
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+            gvPlanes.AutoGenerateColumns = false;
+
+            DataGridViewColumn column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Name";
+            column.Name = "Назва літака";
+            gvPlanes.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Country";
+            column.Name = "Країна";
+            gvPlanes.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Producer";
+            column.Name = "Виробник";
+            gvPlanes.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Seats";
+            column.Name = "Кількість місць";
+            gvPlanes.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Max_Speed";
+            column.Name = "Максимальна швидкість";
+            gvPlanes.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Time";
+            column.Name = "Час польоту";
+            column.Width = 80;
+            gvPlanes.Columns.Add(column);
+
+            column = new DataGridViewCheckBoxColumn();
+            column.DataPropertyName = "Toilet";
+            column.Name = "Туалет";
+            column.Width = 60;
+            gvPlanes.Columns.Add(column);
+
+            column = new DataGridViewCheckBoxColumn();
+            column.DataPropertyName = "Duty_free";
+            column.Name = "Дьюті-фрі";
+            column.Width = 60;
+            gvPlanes.Columns.Add(column);
+
+            bindSrcPlanes.Add(new Plane("Мрія", "Україна", "Володя", 300, 500, 3600, true, false));
+            EventArgs args = new EventArgs(); OnResize(args);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Plane plane = new Plane();
+
+            fPlane ft = new fPlane( plane);
+            if (ft.ShowDialog() == DialogResult.OK)
+            {
+                bindSrcPlanes.Add(plane); 
+            }
+
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            Plane plane = (Plane)bindSrcPlanes.List[bindSrcPlanes.Position];
+
+            fPlane ft = new fPlane( plane);
+            if (ft.ShowDialog() == DialogResult.OK)
+            {
+                bindSrcPlanes.List[bindSrcPlanes.Position] = plane;
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Видалити поточний запис" , "Видалення запису" , MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                bindSrcPlanes.RemoveCurrent();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Очистити таблюцю ? \n \nВсі данні будуть втрачені ", "Очищення данних ", MessageBoxButtons.OKCancel , MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                bindSrcPlanes.Clear();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Закрити застосунок" , "Вихід з програми" , MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void gvPlanes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
